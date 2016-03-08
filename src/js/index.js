269fb35598c24906f701660e6aca176da2e7eba6
BRITTANY ANDREWS <brittanyandrewsnyc@gmail.com>
2016-03-08 18:53:08 -0500

    almost final commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 // Javascript Entry Point
+import $ from 'jquery';
+
+// $('.box') ;
+
+
+$('.link2').on('click', function(){
+	$('.box2').toggleClass('updown');
+
+});
+
+
+$('.link3').on('click', function(){
+	$('.box3').toggleClass('updown2');
+
+});
